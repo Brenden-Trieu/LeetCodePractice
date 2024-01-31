1d6bc0de15a64536ab006037a2d709a9bb61e597
Brenden T <brenden.trieu@gmail.com>
2024-01-30 18:37:56 -0800

    Buy Sell doneish

--- LeetCode/src/BuySell/BuySell.java
+++ LeetCode/src/BuySell/BuySell.java
@@ -2,6 +2,20 @@ package BuySell;
 
 public class BuySell {
     public int maxProfit(int[] prices) {
-        return 0;
+        int max = 0, min = 0;
+        for (int i = 0; i < prices.length; i++) {
+            min = prices[i] < prices[min] ? i : min;
+        }
+        max = min;
+        if (min == prices.length - 1) {
+            return 0;
+        } else {
+            for (int i = min; i < prices.length; i++) {
+                max = prices[i] > prices[max] ? i : max;
+            }
+
+            return prices[max] - prices[min];
+        }
+
     }
 }
