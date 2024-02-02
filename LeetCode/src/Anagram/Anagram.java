b444418d7e0cecc58c8676949b997b8f5a761073
Brenden T <brenden.trieu@gmail.com>
2024-02-01 19:06:39 -0800

    optimized anagram

--- LeetCode/src/Anagram/Anagram.java
+++ LeetCode/src/Anagram/Anagram.java
@@ -9,15 +9,17 @@ public class Anagram {
         Arrays.sort(charArray);
         Arrays.sort(targetArray);
 
-        if (charArray.length == targetArray.length) {
-            for (int i = 0; i < targetArray.length; i++) {
-                if (charArray[i] != targetArray[i])
-                    return false;
-            }
-        } else {
-            return false;
-        }
+        // if (charArray.length == targetArray.length) {
+        // for (int i = 0; i < targetArray.length; i++) {
+        // if (charArray[i] != targetArray[i])
+        // return false;
+        // }
+        // } else {
+        // return false;
+        // }
 
-        return true;
+        // return true;
+
+        return Arrays.equals(charArray, targetArray);
     }
 }
